f4b74d45110ad166971f0b154e55ed4a95b22abe
cooki <17218@students.mrgs.school.nz>
2021-03-09 20:54:31 +1300

    MainActivity.java Quiz part 1 code

--- app/src/main/java/com/bsisou/quiz/MainActivity.java
+++ app/src/main/java/com/bsisou/quiz/MainActivity.java
@@ -2,6 +2,12 @@ package com.bsisou.quiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,15 +15,49 @@ public class MainActivity extends Activity {
 
 
     // TODO: Declare member variables here:
+Button trueButton;
+Button falseButton;
+TextView scoreTextView;
+TextView questionTextView;
+ProgressBar progressBar;
+
 
 
     // TODO: to create question bank
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+        questionTextView = findViewById(R.id.question_text_view);
+        scoreTextView = findViewById(R.id.score);
+        progressBar = findViewById(R.id.progress_bar);
+
+
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Quiz", "True Button Pressed");
+                Toast text_message = Toast.makeText(getApplicationContext(), "true clicked", Toast.LENGTH_SHORT);
+                text_message.show();
+            }
+        });
+
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Quiz", "True Button Pressed");
+            }
+        });
+
+
 
 
 
