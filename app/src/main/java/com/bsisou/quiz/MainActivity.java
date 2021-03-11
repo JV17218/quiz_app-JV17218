d94a2471df1169c3950fcdf2eea0b86be88ebf42
cooki <17218@students.mrgs.school.nz>
2021-03-11 17:44:12 +1300

    MainActivity.java Quiz part 1 and 2 code

--- app/src/main/java/com/bsisou/quiz/MainActivity.java
+++ app/src/main/java/com/bsisou/quiz/MainActivity.java
@@ -20,17 +20,39 @@ Button falseButton;
 TextView scoreTextView;
 TextView questionTextView;
 ProgressBar progressBar;
+//Int variable to retrieve question from an array, and track which question is on display.
+int index;
+//In variable to that hold the question Id
+int question;
+
+
+
 
 
 
     // TODO: to create question bank
+    private TrueFalse[]  questionBank = new com.bsisou.quiz.TrueFalse[] {
+            new TrueFalse(R.string.question_1, false),
+            new TrueFalse(R.string.question_2, false),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, false),
+            new TrueFalse(R.string.question_6, true),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, true),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13, true)
+
+    };
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
 
         trueButton = findViewById(R.id.true_button);
@@ -40,13 +62,13 @@ ProgressBar progressBar;
         progressBar = findViewById(R.id.progress_bar);
 
 
-
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("Quiz", "True Button Pressed");
                 Toast text_message = Toast.makeText(getApplicationContext(), "true clicked", Toast.LENGTH_SHORT);
                 text_message.show();
+                updateQuestion(); // update question after user clicks "True".
             }
         });
 
@@ -54,12 +76,33 @@ ProgressBar progressBar;
             @Override
             public void onClick(View view) {
                 Log.d("Quiz", "True Button Pressed");
+                updateQuestion(); // updates question after user clicks "False".
             }
         });
 
+        question = questionBank[index].getQuestionID();
+        questionTextView.setText(question);
 
+        }
 
+    public void updateQuestion() {
+        index += 1; //so each time we answer (will call this method) and move to next question id in array
+        question = questionBank[index].getQuestionID(); // get the question id of that item in the array
+        questionTextView.setText(question); // sets the question view text to what the question is.
+        index = (index + 1) % questionBank.length; // index 13 doesn't exist, which is what the program was doing.
 
 
     }
-}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/bsisou/quiz/TrueFalse.java
@@ -0,0 +1,39 @@
+package com.bsisou.quiz;
+
+public class TrueFalse {
+
+
+    private int questionID;
+
+
+    private boolean answer;
+
+
+
+
+    public TrueFalse(int questionResourceID, boolean trueOrFalse) {
+
+
+        questionID = questionResourceID;
+        answer = trueOrFalse;
+
+
+
+    }
+
+    public int getQuestionID() {
+        return questionID;
+    }
+
+    public void setQuestionID(int questionID) {
+        this.questionID = questionID;
+    }
+
+    public boolean isAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(boolean answer) {
+        this.answer = answer;
+    }
+}
